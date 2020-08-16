0c47e9371e33440387ee54df950e8c46d0a8857b
Yijun <yijunl@usc.edu>
2020-08-16 10:53:05 -0700

    data loading

--- demo.py
+++ demo.py
@@ -1,21 +1,32 @@
+"""
+This demo shows the simulation example reported in
+[1] J. Li, et al. "Scalable and robust tensor decomposition for brain network identification in spontaneous stereotactic EEG data", IEEE Trans. Biomed. Eng., 2018.
+[2] J. Li, et al. "Robust tensor decomposition of resting brain networks in stereotactic EEG", IEEE 51st Asilomar Conference on Signal, System and Computers, Pacfic Grove, CA, 2017.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 import h5py
 
+R = 5;
+
 data_path = "/hd2/research/brain/time_series/my_SRSCPD_ALS/data.mat"
-# data = scipy.io.loadmat(data_path)
-
-# print(data.shape)
-
 with h5py.File(data_path, 'r') as file:
-    print(file['data'].shape)
-    print(file['data'])
-    X_ref = file['data'][0][0]
-    Xn_ref = file['data'][1][0]
+    X_ref = file['data'][0, 0]
+    Xn_ref = file['data'][1, 0]
+    UGT_ref = file['data'][2, 0]
+    xAxis_ref = file['data'][3, 0]
 
     X = np.array(file[X_ref]).T
     Xn = np.array(file[Xn_ref]).T
-    print(Xn[19, 1])
-plt.imshow(Xn, cmap="gray")
-plt.axis('off')
-plt.show()
+
+    xAxis = file[xAxis_ref]
+    t = np.array(file[xAxis[1, 0]]).T
+    freq = np.array(file[xAxis[2, 0]]).T
+
+print(t.shape)
+print(freq.shape)
+
+# plt.imshow(Xn, cmap="gray")
+# plt.axis('off')
+# plt.show()
