9bd728e608ea0742d850ab6b1bcca44e5593e237
Yijun <yijunl@usc.edu>
2020-08-19 12:23:51 -0700

    format

--- TFOCS_LS.m
+++ TFOCS_LS.m
@@ -1,8 +1,8 @@
 function X2 = TFOCS_LS()
 
-    load('var.mat','A2', 'B2', 'x0');    
-    
-    addpath /hd2/sw1/TFOCS
+    load('var.mat', 'A2', 'B2', 'x0');
+    addpath /hd2/sw1/TFOCS;
+
     optTFOCS = tfocs;
     optTFOCS.tol = 1e-6;
     optTFOCS.restart = 5;
@@ -10,15 +10,14 @@ function X2 = TFOCS_LS()
     optTFOCS.alg = 'AT';
     optTFOCS.printEvery = 0;
     optTFOCS.debug = false;
-   
+
 %   no regularization now
     smoothF = smooth_quad;
     linearF = {A2, -B2};
-                
+
 %   with non-negative constraint
     [X2, ~] = tfocs(smoothF, linearF, proj_Rplus, x0, optTFOCS);
 
     save('tfocs_rst.mat', 'X2');
-    
-end
 
+end
